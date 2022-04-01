512fe2f27df42502c458fe77f831f0dcd3e873c0
Gabo Esquivel <contact@gaboesquivel.com>
2022-04-01 09:49:35 -0600

    feat: indexer function + prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import * as http from 'http'
 import { config } from './config'
-import { log } from './utils/logger'
+import { log } from './library/logger'
+
+const indexer = async () => {
+  log.info('Subscribing to blockchain data ...')
+}
 
 const server = http.createServer((_req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
+  res.end('Service running')
 })
 
+indexer()
 server.listen(config.port, config.hostname, () => log.info(`Server running at http://${config.hostname}:${config.port}/`))
